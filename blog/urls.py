5027fdc1fb8e17c31e723c9fb766d8727e8f9763
Yinglei Zhang <yinglei.zh@gmail.com>
2015-08-25 21:17:24 -0400

    move feed to models.py and create a template for it, and it worked

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
-from . import views, feed
+from . import views
 
 urlpatterns = [
 	url(r'^$', views.BlogIndex.as_view(), name="index"),
-	url(r'^feed/$', feed.LatestPosts(), name="feed"),
+	url(r'^feed/$', views.BlogFeed.as_view(), name="feed"),
 	url(r'^post/(?P<slug>\S+)$', views.BlogDetail.as_view(), name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.syndication.views import Feed
 from . import models
 
-# Create your views here.
+class BlogFeed(generic.ListView):
+	queryset = models.Post.objects.published()[:5]
+	template_name = "blog/feed.html"
+	#paginate_by = 2
+
 class BlogIndex(generic.ListView):
 	queryset = models.Post.objects.published()
 	template_name = "blog/home.html"
-	paginate_by = 2
+	#paginate_by = 5
 
 class BlogDetail(generic.DetailView):
 	model = models.Post
