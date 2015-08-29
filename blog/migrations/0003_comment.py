d2b471cdc1127b7884a66210c09d23c27febeed7
YingleiZhang <yinglei.zh@gmail.com>
2015-08-29 16:30:32 -0400

    add the django_comment app to provide comment feature, and sitll need to customize the layout and remove the fields that are not necessary

---
+++ blog/migrations/0003_comment.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0002_auto_20150820_1543'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('text', models.CharField(max_length=200)),
+                ('post', models.ForeignKey(to='blog.Post')),
+            ],
+        ),
+    ]
